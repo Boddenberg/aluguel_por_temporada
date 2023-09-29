a266902f9d4901b0105d120390bcde32de30b993
Boddenberg <filipeboddenberg@yahoo.com.br>
2023-09-29 00:29:39 -0300

    basic project from spring initializr + hello world

---
+++ src/main/kotlin/JuninWins/Project/TechiTubersApplication.kt
@@ -0,0 +1,15 @@
+package JuninWins.Project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TechiTubersApplication
+
+fun main(args: Array<String>) {
+	runApplication<TechiTubersApplication>(*args)
+
+	println("Hello World (pq não pode faltar)")
+	println("Valeu por estar me ajudando tanto mano!!!! Top, bora desvendar o Kotlin :)")
+
+}
---
+++ src/test/kotlin/JuninWins/Project/TechiTubersApplicationTests.kt
@@ -0,0 +1,13 @@
+package JuninWins.Project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TechiTubersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
