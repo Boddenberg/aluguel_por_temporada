3eee3e3178fd8438761a91ad659ae826e5d6cfa7
Boddenberg <filipeboddenberg@yahoo.com.br>
2023-10-09 19:03:15 -0300

    discount policy enums and trying to construct model

---
+++ src/main/kotlin/JuninWins/Project/enums/DiscountPolicyTypeEnum.kt
@@ -0,0 +1,16 @@
+package JuninWins.Project.enums
+
+enum class DiscountPolicyTypeEnum (val descricao: String) {
+    NONE("Acomodação sem políticas de desconto"),
+    WEEKLY("Política de desconto semanal"),
+    BIWEEKLY("Política de desconto quinzenal"),
+    MONTHLY("Política de desconto mensal"),
+    CUSTOMIZED("Política de desconto customizada");
+
+    companion object {
+        fun fromDescricao(descricao: String): StatusReservaEnum? =
+            StatusReservaEnum.values().find { it.descricao == descricao }
+    }
+
+    override fun toString(): String = descricao
+}
