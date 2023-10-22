ea3b12eb6d82e5c8a125276fca47c6ec50f11201
Boddenberg <filipeboddenberg@yahoo.com.br>
2023-10-22 14:14:36 -0300

    testes

--- src/test/kotlin/JuninWins/Project/controller/BookingControllerTest.kt
+++ src/test/kotlin/JuninWins/Project/controller/BookingControllerTest.kt
@@ -1,5 +1,6 @@
 package JuninWins.Project.controller
 
+import JuninWins.Project.DTO.BookingRequestDTO
 import JuninWins.Project.enums.StatusReservaEnum
 import JuninWins.Project.model.Accommodation
 import JuninWins.Project.model.Booking
@@ -14,6 +15,8 @@ import org.mockito.Mockito.mock
 
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.whenever
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import java.time.LocalDate
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
@@ -23,8 +26,8 @@ internal class BookingControllerTest {
 
     val bookingService : BookingService = mock()
     var bookingController : BookingController? = null
-
-
+    val localDate : LocalDate = LocalDate.of(1996, 1, 18)
+    val localDateAfter : LocalDate = LocalDate.of(1997, 1, 18)
     @Before
     fun setUp() {
         bookingController = BookingController(bookingService = bookingService)
@@ -32,22 +35,41 @@ internal class BookingControllerTest {
 
 
     @Test
-    fun test() {
+    fun `when passed an ID, should return a booking object`() {
         val expectedId : Long = 1
         whenever(bookingService.findBookingById(expectedId)).thenReturn(bookingMock())
         val result = bookingController!!.findBooking(expectedId)
 
+        assertEquals(result.statusCode, HttpStatus.OK)
         assertNotNull(result)
     }
+
+    @Test
+    fun `when passed a GuestID, a AccommodationID and a check_in and check_out date, should return a Booking Object`(){
+        val cpf = "46290103865"
+        val id = 1L
+        val bookingDTO : BookingRequestDTO = BookingRequestDTO(localDate, localDateAfter)
+        val guestMock = mock(Guest::class.java)
+        val accommodationMock = mock(Accommodation::class.java)
+        whenever(bookingService.save(bookingDTO, cpf, id)).thenReturn(bookingMock())
+
+        val result = bookingController!!.saveBooking(bookingDTO, id, cpf)
+
+        assertEquals(result.statusCode, HttpStatus.OK)
+        assertNotNull(result)
+
+    }
+
 
     private fun bookingMock() : Booking {
         val guestMock = mock(Guest::class.java)
         val accommodationMock = mock(Accommodation::class.java)
         val statusReservaMock = mock(StatusReservaEnum::class.java)
-        val localDate : LocalDate = LocalDate.of(1996, 1, 18)
-        val localDateAfter : LocalDate = LocalDate.of(1997, 1, 18)
+
         return Booking(accommodationMock, localDate, localDateAfter, 1, 1.0, guestMock, statusReservaMock)
 
   }
+
+
 
 }
---
+++ src/test/kotlin/JuninWins/Project/controller/GuestControllerTest.kt
@@ -0,0 +1,4 @@
+package JuninWins.Project.controller
+
+class GuestController {
+}
