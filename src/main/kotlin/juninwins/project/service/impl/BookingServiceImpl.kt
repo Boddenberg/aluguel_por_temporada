2320ab6a7e893a79fb36e48449563451b676f0e6
Boddenberg <filipeboddenberg@yahoo.com.br>
2025-01-18 11:03:15 -0300

    ajuste

--- src/main/kotlin/juninwins/project/service/impl/BookingServiceImpl.kt
+++ src/main/kotlin/juninwins/project/service/impl/BookingServiceImpl.kt
@@ -1,15 +1,15 @@
 package juninwins.project.service.impl
 
 import juninwins.project.model.booking.Booking
-import juninwins.project.repository.BookingRepository
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class BookingService(val bookingRepository: BookingRepository) {
+class BookingServiceImpl(val bookingService: BookingService) {
 
     fun findBookingById(id: String): Booking {
         return bookingRepository.findById(id).orElseThrow {
             IllegalArgumentException("Booking with ID $id not found")
         }
     }
+}
